63c616b4153ded5069b01a22cae40040e6cafe03
雷嘉盈 <44772017+EmmaLui@users.noreply.github.com>
2020-04-18 03:40:43 +0800

    更新后的送命题

---
+++ 送命题.py
@@ -0,0 +1,22 @@
+import time
+
+送命题 = input("王晟昊最爱的女人是谁")
+
+if 送命题 in ['雷嘉盈','肥雷','EmmaLui','emma']:
+    print("获得世界上最好的女朋友的一个亲亲")
+    time.sleep(5.20)
+else:
+    time.sleep(1)
+
+    print(" ")
+    print("有事吗？")
+
+    time.sleep(2)
+    print("怒火loading...")
+    print(" ")
+
+    想死 = u"给爷爬"
+    for 一个一个爬 in 想死:
+        time.sleep(2)
+        print(一个一个爬*15)
+    time.sleep(60)
