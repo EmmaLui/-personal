30921ce993457569d4b0fe0a1b465d7f6565360c
雷嘉盈 <44772017+EmmaLui@users.noreply.github.com>
2020-04-18 03:01:37 +0800

    写给男朋友的
    只是一个很简单的代码片段，希望日后可以玩透itchat

---
+++ 送命题.py
@@ -0,0 +1,10 @@
+import time
+
+送命题 = input("王晟昊最爱的女人是谁")
+
+if 送命题 in ['雷嘉盈','肥雷','Emma','emma']:
+    print("获得世界上最好的女朋友的一个亲亲")
+    time.sleep(5.20)
+else:
+    print("给爷爬 "*250)
+    time.sleep(60)
